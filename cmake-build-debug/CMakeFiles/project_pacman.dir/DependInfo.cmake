a0cca1a93e3c27c9a6666ec9bcb6196e38d56762
mahsa <mahsadalir91@gmail.com>
2018-01-30 15:14:29 +0330

    headers and others

---
+++ cmake-build-debug/CMakeFiles/project_pacman.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/mahsa/Desktop/project_pacman/main.c" "/cygdrive/c/Users/mahsa/Desktop/project_pacman/cmake-build-debug/CMakeFiles/project_pacman.dir/main.c.o"
+  "/cygdrive/c/Users/mahsa/Desktop/project_pacman/src/game.c" "/cygdrive/c/Users/mahsa/Desktop/project_pacman/cmake-build-debug/CMakeFiles/project_pacman.dir/src/game.c.o"
+  "/cygdrive/c/Users/mahsa/Desktop/project_pacman/src/physics.c" "/cygdrive/c/Users/mahsa/Desktop/project_pacman/cmake-build-debug/CMakeFiles/project_pacman.dir/src/physics.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../windows/sdl2-include"
+  "../windows/sdl2-gfx-include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project_pacman.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_pacman.dir/src/game.c.o"
+  "CMakeFiles/project_pacman.dir/src/physics.c.o"
+  "CMakeFiles/project_pacman.dir/main.c.o"
+  "project_pacman.pdb"
+  "project_pacman.exe"
+  "libproject_pacman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project_pacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
